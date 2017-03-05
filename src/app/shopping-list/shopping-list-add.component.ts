6f6e10dbbdca73cc60719f8e4ad01cfff27cf6d9
Izabela Holota <iza.holota@gmail.com>
2017-03-05 17:02:28 +0100

    Editing items added

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -15,16 +15,19 @@ export class ShoppingListAddComponent implements OnChanges {
   ngOnChanges(changes) {
     if (changes.item.currentValue === null ){
       this.isAdd = true;
+      this.item = {name: null, amount: null};
     } else {
       this.isAdd = false;
     }
   }
 
   onSubmit(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
     if(!this.isAdd){
+      this.sls.editItem(this.item, newIngredient)
 
     } else {
-      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.item = newIngredient;
       this.sls.addItem(this.item);
     }
 
